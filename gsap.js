dc4c3aee997370daeff7151938aea7fbf8673967
Satyam1203 <nakulbegana191170@gmail.com>
2020-06-13 21:45:07 +0530

    animation on load

---
+++ gsap.js
@@ -0,0 +1,8 @@
+const header = document.querySelector('header');
+const canvas = document.querySelector('.block-dijk');
+
+
+const tl = new TimelineMax();
+
+tl.fromTo(canvas, 1, {height: '0vh'}, {height: '100vh'}, {ease: Power2.easeInOut})
+    .fromTo(header, 1.5, {y:'-800%', opacity: '0'}, {y:'0%', opacity: '1'}, {ease: Power2.easeInOut});
